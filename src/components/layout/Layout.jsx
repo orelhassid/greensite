90db9d7b4dfa10ac3a6751d58fc9a594660b66da
orel hassid <nowayback3d@gmail.com>
2020-09-23 07:32:51 +0300

    first commit

---
+++ src/components/layout/Layout.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import {Header, Content} from '../layout';
+
+export default function Layout() {
+    return (
+        <div className="site-wrapper">
+            <Header />
+            <Content>
+            
+            </Content>
+        </div>
+    )
+}
