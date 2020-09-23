bd8f5c103cdebc7705754abf2da603c96d14551c
orel hassid <nowayback3d@gmail.com>
2020-09-23 07:59:25 +0300

    React-Router-Dom installation and configute

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
 import React from "react";
+import { Switch, Route } from "react-router-dom";
+
+import Home from "./pages/home";
+import HubRegister from "./pages/business/hubRegister";
+
 function App() {
-  return <div>App</div>;
+  return (
+    <Switch>
+      <Route path="/hub/register">
+        <HubRegister />
+      </Route>
+      <Route path="/">
+        <Home />
+      </Route>
+    </Switch>
+  );
 }
 
 export default App;
---
+++ src/pages/business/hubRegister.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Layout, { PageTitle, Content } from "../../components/layout";
+
+function HubRegister() {
+  return (
+    <Layout>
+      <PageTitle />
+      <Content>Register</Content>
+    </Layout>
+  );
+}
+
+export default HubRegister;
