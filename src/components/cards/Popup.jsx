c7c45f14d4bc755a357bb25d94ee63e70f77e43b
orel hassid <nowayback3d@gmail.com>
2020-09-26 10:57:10 +0300

    Fix close popup bug

--- src/components/cards/Popup.jsx
+++ src/components/cards/Popup.jsx
@@ -1,8 +1,28 @@
 import React from "react";
 import "./popup.scss";
 
+import { useHistory, useLocation } from "react-router-dom";
+
+import ButtonIcon from "../elements/ButtonIcon";
+import { ReactComponent as CloseIcon } from "../../assets/icons/close.svg";
+
 const Popup = ({ children }) => {
-  return <article className="popup">{children}</article>;
+  const location = useLocation();
+  const history = useHistory();
+  const closeCard = () => {
+    history.replace({
+      ...location,
+      search: "",
+    });
+  };
+  return (
+    <article className="popup">
+      <div className="close" onClick={() => closeCard()}>
+        <ButtonIcon icon={<CloseIcon />} />
+      </div>
+      {children}
+    </article>
+  );
 };
 
 export default Popup;
--- src/components/cards/TermsCard.jsx
+++ src/components/cards/TermsCard.jsx
@@ -1,28 +1,14 @@
 import React, { useState } from "react";
-import { useHistory, useLocation } from "react-router-dom";
 import Popup from "./Popup";
 import useMarkdown from "../../hooks/useMarkdown";
-import ButtonIcon from "../elements/ButtonIcon";
-import { ReactComponent as CloseIcon } from "../../assets/icons/close.svg";
 // import marked from "marked";
 
 const TermsCard = () => {
   const [markdown, setMarkdown] = useState();
   useMarkdown(setMarkdown);
 
-  const location = useLocation();
-  const history = useHistory();
-  const closeCard = () => {
-    history.replace({
-      ...location,
-      search: "",
-    });
-  };
   return (
     <Popup>
-      <div className="close" onClick={() => closeCard()}>
-        <ButtonIcon icon={<CloseIcon />} />
-      </div>
       <section dangerouslySetInnerHTML={{ __html: markdown }}></section>
     </Popup>
   );
