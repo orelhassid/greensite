6d0ee640cad052272d857dd0450c80f53338f5f6
orel hassid <nowayback3d@gmail.com>
2020-09-27 09:26:23 +0300

    Adding support for apple devices for icons

--- src/components/layout/SEO.js
+++ src/components/layout/SEO.js
@@ -7,6 +7,8 @@ export const SEO = ({ title, description }) => {
       <meta charSet="utf-8" />
       <title>GreenSite | {title}</title>
       <meta name="description" content={description}></meta>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta http-equiv="X-UA-Compatible" content="ie=edge" />
       <link rel="canonical" href="https://orelhassid-greensite.netlify.app/" />
 
       {/* <link
@@ -27,6 +29,9 @@ export const SEO = ({ title, description }) => {
         href="/public/favicon.ico"
         sizes="96x96"
       />
+      <link rel="apple-touch-icon" href="/public/GreenSite-Logo@96w.png" />
+      <meta name="apple-mobile-web-app-status-bar" content="#008725" />
+      <meta name="theme-color" content="#008725" />
     </Helmet>
   );
 };
