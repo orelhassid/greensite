a849559097a576cd730fceb48995054ee3e1a1e5
orel hassid <orelhassid@outlook.com>
2020-09-30 09:05:31 +0300

    Fix Singlke Page Zone

--- src/components/cards/Card.jsx
+++ src/components/cards/Card.jsx
@@ -14,7 +14,6 @@ const Card = ({ zone, link }) => {
   const shareLink = `https://${window.location.hostname}${link}`;
   const colors = ["red", "green", "orange"];
 
-  console.log(shareLink);
   const copyToClipboard = (e) => {
     navigator.clipboard.writeText(shareLink);
     toast.success("Copy to clipboard");
--- src/pages/zones/ZonesSinglePage.jsx
+++ src/pages/zones/ZonesSinglePage.jsx
@@ -9,26 +9,47 @@ import hubService from "../../services/hubService";
 import { useHistory, useParams } from "react-router-dom";
 import Card from "../../components/cards/Card";
 import { HubContext } from "../../contexts/HubContext";
+import Loader from "../../components/elements/Loader";
 
 function ZonesSinglePage() {
   const [loading, setLoading] = useState(false);
   const [zone, setZone] = useState({});
   const history = useHistory();
   const params = useParams();
-  const { hub, zones } = useContext(HubContext);
+  const { hub } = useContext(HubContext);
 
   useEffect(() => {
-    console.log(zones);
-    const data = zones.find((zone) => zone.zone_id === params.zid);
-    setZone(data);
-  }, []);
+    async function fetch() {
+      try {
+        if (hub) {
+          // const zones = await hubService.getZones(hub.hid);
+          const { data } = await hubService.getZone(
+            params.hid,
+            parseInt(params.zid)
+          );
+          setLoading(true);
+          setZone(data);
+        }
+      } catch (error) {}
+    }
+    fetch();
+  }, [hub]);
 
   return (
     <Layout>
       <Navigation />
-      <PageTitle title={hub.name} subtitle="Single Page for QR Code" />
+      {loading ? (
+        <>
+          <PageTitle title={hub.name} subtitle="Single Page for QR Code" />
 
-      <Content>{loading && <Card zone={zone} />}</Content>
+          <Content>
+            {" "}
+            <Card zone={zone} />{" "}
+          </Content>
+        </>
+      ) : (
+        <Loader />
+      )}
     </Layout>
   );
 }
